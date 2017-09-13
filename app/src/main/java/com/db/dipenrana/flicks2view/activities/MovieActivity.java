bc439638685c109dd08bd84977c617d11f7f88ba
drana <dipen.rana@hotmail.com>
2017-09-12 23:12:54 -0700

    added http async and tested response from moviedb with  api key

--- app/src/main/java/com/db/dipenrana/flicks2view/activities/MovieActivity.java
+++ app/src/main/java/com/db/dipenrana/flicks2view/activities/MovieActivity.java
@@ -7,6 +7,12 @@ import android.widget.ListView;
 import com.db.dipenrana.flicks2view.R;
 import com.db.dipenrana.flicks2view.adapters.MoviesAdapter;
 import com.db.dipenrana.flicks2view.models.Movie;
+import com.db.dipenrana.flicks2view.utils.NetworkUtil;
+import com.loopj.android.http.*;
+
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 import java.util.ArrayList;
 
@@ -29,5 +35,24 @@ public class MovieActivity extends AppCompatActivity {
         //attach listview to adpater
         ListView lvMovies = (ListView) findViewById(R.id.lvMovies);
         lvMovies.setAdapter(moviesAdapter);
+
+
+        //setup async http client https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed
+        String url = NetworkUtil.API_URL + "/now_playing?api_key=" + NetworkUtil.API_KEY;
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        params.put("q", "android");
+        params.put("rsz", "8");
+        client.get(url, new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                super.onSuccess(statusCode, headers, response);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                super.onFailure(statusCode, headers, throwable, errorResponse);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/db/dipenrana/flicks2view/utils/NetworkUtil.java
@@ -0,0 +1,17 @@
+package com.db.dipenrana.flicks2view.utils;
+
+/**
+ * Created by dipenrana on 9/12/17.
+ */
+
+public class NetworkUtil {
+
+    public static final String API_KEY = "bf60f19c76b0c89c52708146fab54ba2";
+    public static final String API_URL = "http://api.themoviedb.org/3/movie";
+
+    public static final String BACKDROP_PATH = "http://image.tmdb.org/t/p/w780";
+    public static final String POSTER_PATH = "http://image.tmdb.org/t/p/w185";
+
+    public static final String NETWORK_ERROR_MSG = "Could not connect to the server.\nTry again later.";
+
+}
